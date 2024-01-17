0e0e57d2198b8969119f412c7b1e3ab930bb728e
Nico-Mayer <nico.m94@icloud.com>
2024-01-17 13:29:36 +0100

    Update video capture settings

--- src/main.py
+++ src/main.py
@@ -83,7 +83,9 @@ def handle_keypress(
 
 def main():
     cube = Cube()
-    cap = cv2.VideoCapture(CAM_INDEX)
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) 
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
     ui = UI()
     mode = Mode()
     selected_side = Selected_Side()
